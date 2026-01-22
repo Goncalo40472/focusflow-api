beac0c7928fb6223c2ca083839d004c7f7835c3f
ggraca <goncalo.graca2000@gmail.com>
2026-01-22 13:00:39 +0000

    Add endpoint to return list of tasks

--- src/main/java/com/projects/focusflow/controller/TaskController.java
+++ src/main/java/com/projects/focusflow/controller/TaskController.java
@@ -4,12 +4,22 @@ import com.projects.focusflow.model.Task;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class TaskController {
 
-    @GetMapping("/task")
-    public Task getTask() {
-        return new Task(1L, "Estudar Spring Boot", false);
+    @GetMapping("/tasks")
+    public List<Task> getTasks() {
+
+        List<Task> tasks = new ArrayList<>();
+
+        tasks.add(new Task(1L, "Estudar Spring Boot", false));
+        tasks.add(new Task(2L, "Aprender Git", true));
+        tasks.add(new Task(3L, "Criar projeto FocusFlow", false));
+
+        return tasks;
     }
 
 }
